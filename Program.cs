82fc173c695625bf88495dc0fe8c74c8618529fd
Stefano Calzoni <stefanoclz@hotmail.it>
2022-06-27 14:47:34 +0200

    secondo snack completato

--- Program.cs
+++ Program.cs
@@ -1,7 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 
+/* Calcola la somma e la media dei numeri da 2 a 10 */
+
+int somma = 0;
+
+int media;
+
+for (int i = 2; i <= 10; i++)
+{
+    somma += i;
+
+}
+
+media = somma / (10 - 2);
+
+Console.WriteLine("la somma è: " + somma);
+
+Console.WriteLine("la media è: " + media);
+
+
+return;
 /*Il software deve chiedere per 10 volte all’utente di inserire un numero.
 Il programma stampa la somma di tutti i numeri inseriti.*/
 
