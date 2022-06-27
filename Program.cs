5b10313e4f6e5e09c3ff0d2cbf8364d9581162b7
Stefano Calzoni <stefanoclz@hotmail.it>
2022-06-27 15:19:32 +0200

    esercizio snack 4 completato

--- Program.cs
+++ Program.cs
@@ -1,5 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+/* Fai inserire un numero che chiamaeremo N all'utente, genera N array ognuno formato da 10 numeri casuali tra 1 e 100, ogni volta che ne crei uno stampalo a schermo */
+
+int N = Int32.Parse(Console.ReadLine());
+
+for (int i = 0; i < N; i++)
+{
+    int[] arrayi = new int[10];
+    for(int j = 0; j < 10; j++)
+    {
+        var rand = new Random();
+
+        arrayi[j] = rand.Next(1, 100);
+    }
+    Console.WriteLine("ARRAY" + (i+1));
+    for (int l = 0; l < arrayi.Length; l++)
+    {
+        Console.WriteLine(arrayi[l]);
+    }
+}
+
+
+
+
+
+return;
+
 /* Il software chiede all'utente di inserire un numero, se è pari stampa un numero, se dispari stampa il successivo */
 
 int numeroRichiesto = Int32.Parse(Console.ReadLine());
